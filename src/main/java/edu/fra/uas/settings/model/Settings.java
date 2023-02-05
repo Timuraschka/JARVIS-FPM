ca4bd7f3f70bd0169b46126a3ef0278c3f08e568
Timuraschka <82501522+Timuraschka@users.noreply.github.com>
2023-02-05 17:12:02 +0100

    javax entfernt

--- src/main/java/edu/fra/uas/settings/model/Settings.java
+++ src/main/java/edu/fra/uas/settings/model/Settings.java
@@ -1,14 +1,14 @@
 package edu.fra.uas.settings.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import edu.fra.uas.project.model.Project;
 
