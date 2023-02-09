f8fd9ad1b6228f1d19c7f4877e6b787c2f10f51d
Manu <emmanuel.david@stud.fra-uas.de>
2023-02-09 19:37:16 +0100

    test für PC

--- src/main/java/edu/fra/uas/task/model/Status.java
+++ src/main/java/edu/fra/uas/task/model/Status.java
@@ -2,14 +2,13 @@ package edu.fra.uas.task.model;
 
 public enum Status {
 
-	Backlog ("Backlog"),
-	Ready ("Ready"),
-	In_Progress ("In Progress"),
-	Done ("Done");
+	Backlog("Backlog"),
+	Ready("Ready"),
+	In_Progress("In Progress"),
+	Done("Done");
 
 	Status(String string) {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub lel
 	}
-	
-	
+
 }
