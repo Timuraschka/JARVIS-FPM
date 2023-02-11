a568d3a7136e2fa928b20b75454a93a09ab3d913
Micho <michael.el-dalil@stud.fra-uas.de>
2023-02-11 20:57:27 +0100

    Optimierungen

--- src/main/resources/static/js/AddProject.js
+++ src/main/resources/static/js/AddProject.js
@@ -1,57 +1,64 @@
- var addProjectBtn = document.getElementById("add-project-btn");
- var overlay = document.getElementById("add-project-overlay");
- var closeBtn = document.querySelector(".close-btn");
+// Get references to DOM elements
+var addProjectBtn = document.getElementById("add-project-btn");
+var overlay = document.getElementById("add-project-overlay");
+var closeBtn = document.querySelector(".close-btn");
 
- addProjectBtn.onclick = function() {
-   overlay.style.display = "block";
- }
+// Event listener for the button to open the popup window
+addProjectBtn.onclick = function() {
+overlay.style.display = "block";
+}
 
- closeBtn.onclick = function() {
-   overlay.style.display = "none";
- }
- // Referenzen auf die DOM-Elemente
- ;
- const addProjectForm = document.getElementById("add-project-form");
- const projectTable = document.getElementById("project-table");
- const addProjectOverlay = document.getElementById("add-project-overlay");
+// Event listener for the button to close the popup window
+closeBtn.onclick = function() {
+overlay.style.display = "none";
+}
 
- // Event-Listener für den Button zum Öffnen des Popup-Fensters
- addProjectBtn.addEventListener("click", () => {
-   addProjectOverlay.style.display = "block";
- });
+// Get references to the DOM elements
+const addProjectForm = document.getElementById("add-project-form");
+const projectTable = document.getElementById("project-table");
+const addProjectOverlay = document.getElementById("add-project-overlay");
 
- // Event-Listener für den Button zum Schließen des Popup-Fensters
- closeBtn.addEventListener("click", () => {
-   addProjectOverlay.style.display = "none";
- });
+// Event listener for the button to open the popup window
+addProjectBtn.addEventListener("click", () => {
+addProjectOverlay.style.display = "block";
+});
 
- // Event-Listener für das Absenden des Formulars zum Hinzufügen von Projekten
- addProjectForm.addEventListener("submit", (event) => {
-   // Verhindert das Standardverhalten des Formulars, damit die Seite nicht neu geladen wird
-   event.preventDefault();
-   // Referenzen auf die Eingabefelder
-   const projectLeaderInput = document.getElementById("project-leader");
-   const taskInput = document.getElementById("task");
-   const statusInput = document.getElementById("status");
-   const deadlineInput = document.getElementById("deadline");
-   // Erstellt eine neue Tabellenzeile mit den Daten aus den Eingabefeldern
-   const newRow = projectTable.insertRow(0);
-   newRow.innerHTML = `
-    <td><a href="#">${projectLeaderInput.value}</a></td>
-    <td>${taskInput.value}</td>
-    <td>${statusInput.value}</td>
-    <td>${deadlineInput.value}</td>
-  `;
+// Event listener for the button to close the popup window
+closeBtn.addEventListener("click", () => {
+addProjectOverlay.style.display = "none";
+});
 
-   // Fügt die neue Zeile am Anfang der Tabelle hinzu
-   projectTable.querySelector("tbody").insertBefore(newRow, projectTable.querySelector("tbody").firstChild);
+// Event listener for the form submit event
+addProjectForm.addEventListener("submit", (event) => {
+	
+// Prevent the form from submitting and refreshing the page
+event.preventDefault();
 
-   // Leert die Eingabefelder
-   projectLeaderInput.value = "";
-   taskInput.value = "";
-   statusInput.value = "";
-   deadlineInput.value = "";
-   // Schließt das Popup-Fenster
-   addProjectOverlay.style.display = "none";
+// Get references to the input fields
+const projectLeaderInput = document.getElementById("project-leader");
+const projectTeamInput = document.getElementById("project-team");
+const projectTitleInput = document.getElementById("project-title");
+const deadlineInput = document.getElementById("deadline");
 
- });
+// Create a new table row with the input field data
+const newRow = projectTable.insertRow(0);
+newRow.innerHTML = `
+<td>${projectLeaderInput.value}</td> 
+<td>${projectTeamInput.value}</td> 
+<td>${projectTitleInput.value}</td> 
+<td>${deadlineInput.value}</td> 
+`;
+// Add the new row at the beginning of the table
+
+projectTable.querySelector("tbody").insertBefore(newRow, projectTable.querySelector("tbody").firstChild);
+
+// Clear the input fields
+
+	projectLeaderInput.value = "";
+	projectTeamInput.value = "";
+	projectTitleInput.value = "";
+	deadlineInput.value = "";
+
+// Close the popup window
+	addProjectOverlay.style.display = "none";
+});
