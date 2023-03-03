512f5c00d15e421cb0b91d35012666155f6ff0c5
Manu <emmanuel.david@stud.fra-uas.de>
2023-03-03 17:49:20 +0100

    API Controller

--- src/main/java/edu/fra/uas/controller/ApiController.java
+++ src/main/java/edu/fra/uas/controller/ApiController.java
@@ -1,6 +1,5 @@
 package edu.fra.uas.controller;
 
-
 import java.net.URI;
 import java.util.Collection;
 import java.util.List;
@@ -36,56 +35,90 @@ import edu.fra.uas.user.service.UserService;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-	
 
 	private static final Logger log = LoggerFactory.getLogger(ApiController.class);
-	
+
 	private UserService userS;
 	private TokenService tokenS;
 	private TaskService taskS;
 	private ProjectService projectS;
 	private ResouceService resourceS;
 	private TimertrackerService timeS;
-	
-	
+
 	@Autowired
-	public ApiController (UserService userS, TaskService taskS, ProjectService projectS, 
-			ResouceService resourceS, TimertrackerService timeS) 
-	{
-		
+	public ApiController(UserService userS, TaskService taskS, ProjectService projectS,
+			ResouceService resourceS, TimertrackerService timeS) {
+
 		this.userS = userS;
 		this.taskS = taskS;
 		this.projectS = projectS;
 		this.resourceS = resourceS;
 		this.timeS = timeS;
-		
-	}
-	
-	@GetMapping
-	public ResponseEntity<String> getLogin(){
-		return new ResponseEntity<String>("login", HttpStatusCode.valueOf(200));
+
 	}
 
+	@GetMapping
+	public ResponseEntity<String> getLogin() {
+		return new ResponseEntity<String>("login", HttpStatusCode.valueOf(200));
+	}
 
 	/**
 	 * 
 	 */
-	@RequestMapping(value = "/user/{token}")
-	public ResponseEntity<List<ProjectDTO>> getProjects (@PathVariable("token") String token){
-		
+	@RequestMapping(value = "/user/{user_id}/{token}/project", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<ProjectDTO>> getProjects(@PathVariable("user_id") long user_id,
+			@PathVariable("token") String token) {
+
 		User u = userS.getUserWithToken(token);
 		List<ProjectDTO> projects = projectS.getProjectsForUser(u);
-		
+
 		List<ProjectDTO> collection = projects;
 		return new ResponseEntity<List<ProjectDTO>>(collection, HttpStatusCode.valueOf(200));
 	}
-	
-	
+
+	@RequestMapping(value = "/user/{token}/project/{project_id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List> getKanbanWBS(@PathVariable("token") String token,
+			@PathVariable("project_id") long project_id) {
+
+		return null;
+	}
+
+	@RequestMapping(value = "/user/{token}/project/{project_id}/tasks/{task_id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List> getKanbanWBSTask(@PathVariable("token") String token,
+			@PathVariable("project_id") long project_id, @PathVariable("task_id") long task_id) {
+
+		return null;
+	}
+
+	@RequestMapping(value = "/user/{token}/project/{project_id}/tasks/{task_id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List> putKanbanWBStask(@PathVariable("token") String token,
+			@PathVariable("project_id") long project_id, @PathVariable("task_id") long task_id) {
+
+		return null;
+	}
+
+	@RequestMapping(value = "/user/{token}/project/{project_id}/tasks/{task_id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List> postWBSTask(@PathVariable("token") String token,
+			@PathVariable("project_id") long project_id, @PathVariable("task_id") long task_id) {
+		return null;
+	}
+
+	@RequestMapping(value = "/user/{token}/project/{project_id}/resource", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List> getResource(@PathVariable("token") String token,
+			@PathVariable("project_id") long project_id) {
+		return null;
+	}
+
+	@RequestMapping(value = "/user/{token}/project/{project_id}/resource/{resource_id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List> postResource(@PathVariable("token") String token,
+			@PathVariable("project_id") long project_id, @PathVariable("resource_id") long resource_id) {
+		return null;
+	}
+
+	@RequestMapping(value = "/user/{token}/project/{project_id}/resource/{resource_id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List> putResource(@PathVariable("token") String token,
+			@PathVariable("project_id") long project_id, @PathVariable("resource_id") long resource_id) {
+		return null;
+	}
+
 }
-
-
-
-
-
-
-
