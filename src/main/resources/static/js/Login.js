345b91e6546c93147f330805da1023a2d5033469
Sxmuel-szucki <s.szucki@gmail.com>
2023-03-03 18:38:43 +0100

    hi

--- src/main/resources/static/js/Login.js
+++ src/main/resources/static/js/Login.js
@@ -1,32 +1,32 @@
 
-  const form = document.querySelector('.login-form');
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const username = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
 
-    //Endpunkt mit Login/Check muss angepasst werden
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/login/check', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        const response = JSON.parse(xhr.responseText);
-        if (response.status === 'success') {
-          // Weiterleitung zur erfolgreichen Anmeldung
-          window.location.href = '/home';
-        } else {
-          // Anzeige einer Fehlermeldung
-          alert(response.message);
-        }
+  //Endpunkt mit Login/Check muss angepasst werden
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/login/check', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      if (response.status === 'success') {
+        // Weiterleitung zur erfolgreichen Anmeldung
+        window.location.href = '/home';
       } else {
-        alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.');
+        // Anzeige einer Fehlermeldung
+        alert(response.message);
       }
-    };
-    xhr.onerror = function() {
+    } else {
       alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.');
-    };
-    xhr.send(JSON.stringify({username: username, password: password}));
-  });
+    }
+  };
+  xhr.onerror = function() {
+    alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.');
+  };
+  xhr.send(JSON.stringify({username: username, password: password}));
+});
 
