45a210895835d41aaa6ec89f232bcfa8b52d9432
Sxmuel-szucki <s.szucki@gmail.com>
2023-03-04 15:55:54 +0100

    comments

--- src/main/resources/static/js/Login.js
+++ src/main/resources/static/js/Login.js
@@ -1,31 +1,43 @@
-
 const form = document.querySelector('.login-form');
+
+// add event listener to login form
 form.addEventListener('submit', function(event) {
+  // prevent form from submitting
   event.preventDefault();
-  
+
+  // get username and password from input fields
   const username = document.querySelector('#username').value;
   const password = document.querySelector('#password').value;
 
+  // send login credentials to server
   const xhr = new XMLHttpRequest();
   xhr.open('POST', '/login/check', true);
   xhr.setRequestHeader('Content-Type', 'application/json');
+
+  // handle server response
   xhr.onload = function() {
+    // if login was successful, redirect to home page
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
       if (response.status === 'success') {
-        // Weiterleitung zur erfolgreichen Anmeldung
+        // redirect to home page
         window.location.href = '/home';
       } else {
-        // Anzeige einer Fehlermeldung
+        // show error message
         alert(response.message);
       }
     } else {
-      alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.');
+      // show error message
+      alert('An error occurred. Please try again.');
     }
   };
+
+  // handle network error
   xhr.onerror = function() {
-    alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.');
+    // show error message
+    alert('An error occurred. Please try again.');
   };
+
+  // send credentials to server
   xhr.send(JSON.stringify({username: username, password: password}));
 });
-
--- src/main/resources/static/js/WBS.js
+++ src/main/resources/static/js/WBS.js
@@ -1,10 +1,14 @@
+// Get the current date and format it in German (dd.mm.yyyy)
 let date = new Date().toLocaleDateString("de-DE");
+// Update the HTML element with the id "date" to display the current date
 document.getElementById("date").innerHTML = date;
 
+// Get HTML elements for the timeline start, end, and bar
 let timelineStart = document.getElementById("timeline-start");
 let timelineEnd = document.getElementById("timeline-end");
 let timelineBar1 = document.getElementById("timeline-bar-1");
 
+// Get HTML elements for the task list and input fields
 let taskList = document.getElementById("task-list");
 let taskName = document.getElementById("task-name");
 let taskStartDate = document.getElementById("task-start-date");
@@ -21,9 +25,12 @@ let taskDependentDetails = document.getElementById("task-dependent-details");
 let taskCostsDetails = document.getElementById("task-costs-details");
 let taskBeschreibungDetails = document.getElementById("task-definition-details");
 
+// Initialize an empty array to store tasks
 let tasks = [];
 
+// Function to add a new task to the task list
 function addTask() {
+    // Create a new task object with input field values
     let task = {
         name: taskName.value,
         startDate: taskStartDate.value,
@@ -33,75 +40,85 @@ function addTask() {
         costs: taskCosts.value,
         beschreibung: taskBeschreibung.value
     };
-
+    // Add the new task to the tasks array
     tasks.push(task);
-
+    // Create a new HTML row to display the task in the task list
     let taskRow = `
-    <tr onclick="showTaskDetails(${tasks.length - 1})">
-        <td>${task.name}</td>
-        <td>${task.name}</td>
-        <td>${task.startDate}</td>
-        <td>${task.endDate}</td>
-        <td>${task.resources}</td>
-        <td>${task.dependent}</td>
-        <td>${task.costs}</td>
-    </tr>
-`;
+<tr onclick="showTaskDetails(${tasks.length - 1})">
+    <td>${task.name}</td>
+    <td>${task.name}</td>
+    <td>${task.startDate}</td>
+    <td>${task.endDate}</td>
+    <td>${task.resources}</td>
+    <td>${task.dependent}</td>
+    <td>${task.costs}</td>
+</tr>`;
+    // Add the new task row to the task list HTML element
     taskList.innerHTML += taskRow;
-
+    // Create a new timeline bar HTML element for the new task
     let timelineBarNew = document.createElement("div");
     timelineBarNew.className = "bar";
     timelineBarNew.style.width = `${task.duration * 10}px`;
     timelineBarNew.setAttribute("id", `timeline-bar-${tasks.length}`);
 
+    // Create a label for the timeline bar with the task name
     let labelBarNew = document.createElement("div");
     labelBarNew.className = "label-bar";
     labelBarNew.innerHTML = task.name;
     timelineBarNew.appendChild(labelBarNew);
 
+    // Add the new timeline bar HTML element to the timeline
     let timeline = document.querySelector(".timeline");
     timeline.appendChild(timelineBarNew);
 
+    // Update the timeline end date if the new task end date is later than the current end date
     updateTimelineEnd(new Date(task.endDate));
-}
-function showTaskDetails(index) {
-    taskNameDetails.innerHTML = tasks[index].name;
-    taskStartDateDetails.innerHTML = tasks[index].startDate;
-    taskEndDateDetails.innerHTML = tasks[index].endDate;
-    taskResourcesDetails.innerHTML = tasks[index].resources;
-    taskDependentDetails.innerHTML = tasks[index].dependent;
-    taskCostsDetails.innerHTML = tasks[index].costs;
-    taskBeschreibungDetails.innerHTML = tasks[index].beschreibung;
+    // This function displays the details of a task
+    function showTaskDetails(index) {
+        // Get the task details from the tasks array based on the index
+        taskNameDetails.innerHTML = tasks[index].name;
+        taskStartDateDetails.innerHTML = tasks[index].startDate;
+        taskEndDateDetails.innerHTML = tasks[index].endDate;
+        taskResourcesDetails.innerHTML = tasks[index].resources;
+        taskDependentDetails.innerHTML = tasks[index].dependent;
+        taskCostsDetails.innerHTML = tasks[index].costs;
+        taskBeschreibungDetails.innerHTML = tasks[index].beschreibung;
 
-    let timelineBarOld = document.getElementById("timeline-bar-selected");
-    if (timelineBarOld) {
-        timelineBarOld.removeAttribute("id");
+        // Set the "timeline-bar-selected" ID to the selected timeline bar and remove it from the old selected bar
+        let timelineBarOld = document.getElementById("timeline-bar-selected");
+        if (timelineBarOld) {
+            timelineBarOld.removeAttribute("id");
+        }
+        let timelineBars = document.getElementsByClassName("bar");
+        timelineBars[index].setAttribute("id", "timeline-bar-selected");
+
+        // Show the task details window
+        document.getElementById("task-details").style.display = "block";
     }
-    let timelineBars = document.getElementsByClassName("bar");
-    timelineBars[index].setAttribute("id", "timeline-bar-selected");
-    // sichtbar machen des Task Details-Fensters
-    document.getElementById("task-details").style.display = "block";
-}
 
-
-
-function getDayDifference(date1, date2) {
-    let timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    let dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    return dayDiff;
-}
-
-function updateTimelineEnd(date) {
-    let currentTimelineEnd = new Date(timelineEnd.innerHTML);
-    if (date > currentTimelineEnd) {
-        timelineEnd.innerHTML = date.toISOString().slice(0, 10);
-        timelineBar1.style.width = `${getDayDifference(new Date(timelineEnd.innerHTML), new Date(timelineStart.innerHTML)) * 10}px`;
+    // This function calculates the difference in days between two dates
+    function getDayDifference(date1, date2) {
+        let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        let dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        return dayDiff;
     }
-}
-function openPopup() {
-    document.getElementById("popup").style.display = "block"; a
-}
 
-function closePopup() {
-    document.getElementById("popup").style.display = "none";
+    // This function updates the timeline end date and width of the timeline bar if a task end date is after the current timeline end date
+    function updateTimelineEnd(date) {
+        let currentTimelineEnd = new Date(timelineEnd.innerHTML);
+        if (date > currentTimelineEnd) {
+            timelineEnd.innerHTML = date.toISOString().slice(0, 10);
+            timelineBar1.style.width = `${getDayDifference(new Date(timelineEnd.innerHTML), new Date(timelineStart.innerHTML)) * 10}px`;
+        }
+    }
+
+    // This function opens the popup
+    function openPopup() {
+        document.getElementById("popup").style.display = "block";
+    }
+
+    // This function closes the popup
+    function closePopup() {
+        document.getElementById("popup").style.display = "none";
+    }
 }
